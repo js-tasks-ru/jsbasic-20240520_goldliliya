9faa4a24515245ac9a07ccc3fec08a47e5406278
liliya <goldliliya@mail.ru>
2024-06-11 16:14:42 +0300

    4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,17 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement('ul');
+
+  for (const friend of friends) {
+    const item = document.createElement('li');
+
+    item.textContent = `${friend.firstName} ${friend.lastName}`;
+    list.appendChild(item);
+  }
+
+  // второй вариант
+  // friends.forEach(friend => {
+  //   list.innerHTML += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  // });
+
+  return list;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
+function addClassList (element, className) {
+  element.classList.add(className);
+}
+
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let rows = table.rows[i];
+    let statusCell = rows.cells[3];
+    let genderCell = rows.cells[2].textContent;
+    let ageCell = parseInt(rows.cells[1].textContent, 10);
+
+    if (statusCell.hasAttribute('data-available')) {
+      if (statusCell.getAttribute('data-available') === 'true'){
+        addClassList(rows, 'available');
+      } else {
+        addClassList(rows, 'unavailable');
+      }
+    } else {
+      rows.setAttribute('hidden', 'true');
+    }
+
+    if (genderCell === 'm') {
+      addClassList(rows, 'male');
+    } else {
+      addClassList(rows, 'female');
+    }
+
+    if (ageCell < 18){
+      rows.style.textDecoration = 'line-through';
+    }
+  }
 }
