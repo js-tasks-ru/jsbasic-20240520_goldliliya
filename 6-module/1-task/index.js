0a0c1679b832255178cfd3662e768e687c00195a
liliya <goldliliya@mail.ru>
2024-06-23 23:28:02 +0300

    6-module: task 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,4 +1,16 @@
 /**
+ * Функция для создания DOM-элемента
+ * @param htmlString - строка с HTML-кодом
+ * @returns {Element} - готовый DOM-элемент
+ */
+function createElementFromHTML(htmlString) {
+  const template = document.createElement('div');
+  template.innerHTML = htmlString;
+
+  return template.firstElementChild;
+}
+
+/**
  * Компонент, который реализует таблицу
  * с возможностью удаления строк
  *
@@ -13,6 +25,57 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  #rows = [];
+
   constructor(rows) {
+    this.rows = rows || this.#rows;
+    this.elem = this.#renderTable();
+  }
+
+  #templateRow(row) {
+    return `
+        <tr>
+            <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button>X</button></td>
+        </tr>`;
+  }
+
+  #templateTable() {
+    return `
+      <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+            ${this.rows.map(row => this.#templateRow(row)).join('')}
+        </tbody>
+      </table>
+    `;
+  }
+
+  #onDeleteButton (btn) {
+    return btn.addEventListener('click', () => {
+      btn.parentElement.parentElement.remove();
+    });
+  }
+
+  #renderTable() {
+    this.elem = createElementFromHTML(this.#templateTable());
+
+    for (let btn of this.elem.querySelectorAll('button')) {
+      this.#onDeleteButton(btn);
+    }
+
+    return this.elem;
   }
 }
